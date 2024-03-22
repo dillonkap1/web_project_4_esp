4650d2fe742c99a7fb74e1e4f25ba9eeedd7e5a6
David <davidjara55@outlook.com>
2024-03-22 14:05:41 -0500

    Proyecto corregido 2

--- webpack.config.js
+++ webpack.config.js
@@ -38,13 +38,16 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-                type: "asset/resource"   
+                type: "asset/resource" ,
+                generator: {
+                    filename: "images/[name][ext][query]"
+                }  
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/index.html"
+            template: "./index.html"
         }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin()
